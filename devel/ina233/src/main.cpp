85b3e8bc3800285699b619d2b1e8a396e5f6f512
Jerry Pham <chalizardmisadon@yahoo.com>
2019-09-04 00:48:52 -0400

    simple test code to get ina233 model name

--- devel/ina233/src/main.cpp
+++ devel/ina233/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include "ina233.h"
 
 #define LED_PIN_0 PA0
 #define LED_PIN_1 PA1
@@ -7,16 +8,20 @@
 #define I2C_SCL PB10
 #define I2C_SDA PB11
 
+INA233 ina233(0x40);
+
 void setup()
 {
 	pinMode(LED_PIN_0, OUTPUT);
 	pinMode(LED_PIN_1, OUTPUT);
-	Wire.begin(I2C_SDA, I2C_SCL);
+	Wire.setSCL(I2C_SCL);
+	Wire.setSDA(I2C_SDA);
+	Wire.begin();
 }
 
 void loop()
 {
-	for (int addr = 0; addr <= 127; addr++) {
+	for (int addr = 1; addr < 127; addr++) {
 
 		static bool led_state = false;
 		led_state = !led_state;
@@ -27,12 +32,21 @@ void loop()
 
 		Wire.beginTransmission(addr);
 		if (0 == Wire.endTransmission()) {
+
+			uint8_t *device = ina233.getModel();
+
 			while (1) {
 				led_state = !led_state;
 				digitalWrite(LED_PIN_0, led_state);
+
+				if (addr == 0x40)
+					digitalWrite(LED_PIN_1, HIGH);
+				else digitalWrite(LED_PIN_1, LOW);
+
 				delay(500);
 			}
 		}
 	}
 	delay (5000);
 }
+
